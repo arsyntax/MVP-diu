ce8ffac9c981c6faf79ae466772e2c7e91577fc9
Johannsonmanson <johannvasquez101@gmail.com>
2024-11-16 17:18:25 -0300

    cambio en componente de encuesta

---
+++ src/Components/Teams/EncuestaCard.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import { useNavigate } from 'react-router-dom';
+
+export function EncuestaCard({ id, encuesta }) {
+    const navigate = useNavigate();
+
+    const handleResponder = () => {
+        navigate(`/encuesta/${id}/responder`);
+    }
+    const handleRecordar = () => {
+        alert("Función no implementada");
+    }
+    const handleNoResponder = () => {
+        alert("Función no implementada");
+    }
+
+    return (
+        <div className="encuestaItem">
+            <p>{encuesta.titulo} - {encuesta.equipo}</p>
+            <div className="encuestaOptions">
+                <p onClick={handleResponder} className="encuestaOption">Responder</p> |
+                <p onClick={handleRecordar} className="encuestaOption">Recordar después</p> |
+                <p onClick={handleNoResponder} className="encuestaOption">No responder</p>
+            </div>
+        </div>
+    );
+}
--- src/Components/Teams/Encuestas.jsx
+++ src/Components/Teams/Encuestas.jsx
@@ -1,39 +1,21 @@
 import "./Encuestas.css";
-import { useNavigate } from 'react-router-dom';
-import { Button } from "react-bootstrap";
+import { EncuestaCard } from "./EncuestaCard";
 
 export default function Encuestas() {
-    const navigate = useNavigate();
     const encuestasDisponibles = [
-        { titulo: "Encuesta mes Noviembre", equipo: "Kingsoftware" },
-        { titulo: "Encuesta xd", equipo: "equipo2" }
+        { titulo: "Encuesta mes Noviembre", equipo: "Kingsoftware", respondida: false },
+        { titulo: "Encuesta xd", equipo: "equipo2", respondida: false }
     ];
-
-    const handleResponder = () => {
-        navigate("/encuesta/responder");
-    }
-
-    const handleRecordar = () => {
-        alert("Función no implementada");
-    }
-    const handleNoResponder = () => {
-        alert("Función no implementada");
-    }
     return (
         <div className="encuestasContainer">
             <div className="encuestasTitle">Encuestas pendientes</div>
             <div className="encuestasList">
                 {encuestasDisponibles.length > 0 ? (
                     encuestasDisponibles.map((encuesta, index) => (
-                        <div key={index} className="encuestaItem">
-                            <p>{encuesta.titulo} - {encuesta.equipo}</p>
-                            <div className="encuestaOptions">
-                                <p onClick={handleResponder} className="encuestaOption">Responder</p> |
-                                <p onClick={handleRecordar} className="encuestaOption"> Recordar despues</p> |
-                                <p onClick={handleRecordar} className="encuestaOption">  No responder</p>
-                                
-                            </div>
-                        </div>
+                        (!encuesta.respondida ? 
+                            <EncuestaCard key={index} id={index} encuesta={encuesta} />
+                            : <></>
+                        )
                     ))
                 ) : (
                     <p>No hay encuestas pendientes</p>
